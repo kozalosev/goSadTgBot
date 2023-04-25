79bdfb3201c72bf8056f72c553beb5e8de75914e
Leonid Kozarin <kozalo@sadbot.ru>
2023-04-25 21:51:00 +0300

    [doc] Fix references to base.MessageHandler

--- wizard/doc.go
+++ wizard/doc.go
@@ -2,7 +2,7 @@
 Package wizard provides support for field based forms defined by client code. A key-value storage is used to save
 the state of a form between messages.
 
-To add a form to your [github.com/kozalosev/SadFavBot/base.MessageHandler], it must implement the [WizardMessageHandler]
-interface and create a [Wizard] in its [github.com/kozalosev/SadFavBot/base.MessageHandler.Handle] method.
+To add a form to your [github.com/kozalosev/goSadTgBot/base.MessageHandler], it must implement the [WizardMessageHandler]
+interface and create a [Wizard] in its [github.com/kozalosev/goSadTgBot/base.MessageHandler.Handle] method.
 */
 package wizard
