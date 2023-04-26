849ca9d31f68da6e88fe50653a88390dd974da76
Leonid Kozarin <kozalo@sadbot.ru>
2023-04-26 09:55:23 +0300

    Add a docstring to OptionsFetcher

--- settings/settings.go
+++ settings/settings.go
@@ -7,6 +7,8 @@ type LangCode string
 // UserOptions stored in the database.
 type UserOptions any
 
+// OptionsFetcher is an interface which you should implement if you want to have your specific options to be
+// injected into the [github.com/kozalosev/goSadTgBot/base.RequestEnv] struct automatically.
 type OptionsFetcher interface {
 	FetchUserOptions(uid int64, defaultLang string) (LangCode, UserOptions)
 }
